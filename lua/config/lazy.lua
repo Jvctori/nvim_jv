922a2675887f4e84dfba6520494acc763dedc407
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-11-23 04:12:21 -0300

    .

---
+++ lua/config/lazy.lua
@@ -0,0 +1,85 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+vim.api.nvim_create_user_command("Pmfm", function()
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, {
+    "package main",
+    "",
+    "func main() {",
+    "\t",
+    "}",
+  })
+  vim.api.nvim_win_set_cursor(0, { 4, 1 })
+end, {})
+vim.keymap.set("n", "<Tab>", ">>", { noremap = true, silent = true })
+-- Define leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.o.clipboard = "unnamedplus"
+-- Mostrar número absoluto na linha atual e relativo nas outras
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Desindentar linha no modo normal
+vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true, silent = true })
+
+-- Indentar seleção no modo visual
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+
+-- Desindentar seleção no modo visual
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+-- Default global (opcional)
+vim.opt.expandtab = false -- Go usa tabs reais
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
