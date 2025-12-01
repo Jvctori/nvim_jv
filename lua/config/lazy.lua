38aa8832922bcb4cebb163e2ee96a60c4e55e72d
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-12-01 00:43:24 -0300

    ...

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,19 +30,6 @@ vim.o.clipboard = "unnamedplus"
 -- Mostrar número absoluto na linha atual e relativo nas outras
 vim.opt.number = true
 vim.opt.relativenumber = true
--- tab options
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = false -- Go usa TAB real
-
-vim.opt.list = true
-vim.opt.listchars = {
-  tab = "→ ", -- seta indicando tab
-  trail = "•",
-  precedes = "«",
-  extends = "»",
-}
-
 -- GRUVBOX CONFIGS
 vim.o.background = "dark"
 require("lazy").setup({
