2155a55af2e34d91d6165d50ce9ad6025fd7ad02
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-12-06 14:41:49 -0300

    .

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
-  config = true,
   opts = {
     colorscheme = "gruvbox",
     contrast = "hard",
     transparent_mode = false,
   },
+  config = function(_, opts)
+    require("gruvbox").setup(opts)
+    vim.cmd([[colorscheme gruvbox]])
+  end,
 }
