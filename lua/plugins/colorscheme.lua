0f5538a37980b7c1fc45eac12131dea57b234280
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-12-06 14:40:12 -0300

    .

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,10 @@
-return { "rebelot/kanagawa.nvim" }
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = true,
+  opts = {
+    colorscheme = "gruvbox",
+    contrast = "hard",
+    transparent_mode = false,
+  },
+}
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,20 +2,12 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      go = { "goimports", "gofumpt" }, -- goimports organiza imports, gofumpt formata
-      -- ou use apenas um:
-      -- go = { "goimports-reviser", "gofumpt" },
-    },
-
-    -- Formata automaticamente ao salvar
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
+      go = { "goimports", "gofumpt" },
     },
 
     formatters = {
       goimports = {
-        prepend_args = { "-local", "github.com/seu-usuario" }, -- Opcional: prioriza imports locais
+        prepend_args = { "-local", "github.com/seu-usuario" }, -- Opcional
       },
     },
   },
